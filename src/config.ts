90b4aa8e061c4dd2b61bf4215f5b591d73431ffe
kittimonk <kittimonk393@hotmail.com>
2025-07-09 21:30:08 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,10 @@
 const LOCAL_API_URL = 'http://localhost:8000';
 
 // For Azure Web App environment
-const AZURE_API_URL = '/api'; 
+const AZURE_API_URL = ''; #No /api prefix
 
 // Determine if running on Azure Web App
-const isAzureEnvironment = window.location.hostname.includes('azurewebsites.net');
+const isAzureEnvironment = window.location.hostname.includes('azurewebsites.net')  || window.location.hostname === "kme03.dev.com";
 
 // For Lovable preview environment
 const isLovableEnvironment = window.location.hostname.includes('lovableproject.com') || window.location.hostname.includes('lovable.app');
